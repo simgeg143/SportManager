e407d5e8f2f79b08377aebf9cfca03758eada94a
Willdone-ina <vil_in@ymail.com>
2026-05-09 13:57:55 +0300

    Adding new sport:Basketball
    Signed-off-by: Willdone-ina <vil_in@ymail.com>

--- src/main/java/com/sportmanager/basketball/BasketballTeam.java
+++ src/main/java/com/sportmanager/basketball/BasketballTeam.java
@@ -1,4 +1,63 @@
 package com.sportmanager.basketball;
 
-public class BasketballTeam {
+import com.sportmanager.core.Lineup;
+import com.sportmanager.core.Player;
+import com.sportmanager.core.Team;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+
+public class BasketballTeam extends Team {
+
+    static final int LINEUP_SIZE = 5;
+    static final int MAX_SUBS = 7;
+
+    private static final Map<String, Integer> POSITION_PRIORITY = Map.of(
+            "PG", 0,
+            "SG", 1,
+            "SF", 2,
+            "PF", 3,
+            "C", 4
+    );
+
+    public BasketballTeam(String name) {
+        super(name);
+        this.currentTactic = BasketballTactics.DEFAULT_OFFENSE;
+    }
+
+    @Override
+    public int getRequiredLineupSize() {
+        return LINEUP_SIZE;
+    }
+
+    @Override
+    public int getMaxSubstituteCount() {
+        return MAX_SUBS;
+    }
+
+    @Override
+    public void generateDefaultLineup() {
+        startingLineup.clear();
+        substitutes.clear();
+
+        List<Player> sorted = roster.stream()
+                .filter(Player::isAvailable)
+                .sorted(Comparator
+                        .comparingInt((Player p) -> POSITION_PRIORITY.getOrDefault(p.getPosition(), 99))
+                        .thenComparingInt(p -> -p.getSkillLevel()))
+                .toList();
+
+        for (Player p : sorted) {
+            if (startingLineup.size() < LINEUP_SIZE) {
+                startingLineup.add(p);
+            } else if (substitutes.size() < MAX_SUBS) {
+                substitutes.add(p);
+            } else {
+                break;
+            }
+        }
+
+        currentLineup = new Lineup(startingLineup, substitutes);
+    }
 }
